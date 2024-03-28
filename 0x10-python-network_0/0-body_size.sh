fffb1e20189bde2f7d741df8eebabc507d3afbf8
Naol-Solomon <naolsolomon10@gmail.com>
2024-03-28 12:36:55 -0700

    done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 #print size of response from server
-curl -s -o /dev/null -w "%{size_download}" http://localhost:5000
+
+if [[ $# -ne 1 ]]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+url=$1
+
+# Get the Content-Length from the headers first (more efficient)
+size=$(curl -sI "$url" | awk '/Content-Length:/{print $2}')
+
+# If Content-Length is not available, download the entire body and count bytes
+if [[ -z "$size" ]]; then
+  size=$(curl -s "$url" | wc -c)
+fi
+
+echo "Size of response body: $size bytes"
+
